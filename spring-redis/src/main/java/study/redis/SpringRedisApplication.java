9f2acabe4423503ec1fb86adabbee4389bb67e07
이현수 <znight1020@naver.com>
2025-02-11 23:28:04 +0900

    init: redis 활용 예제 프로젝트 생성

---
+++ spring-redis/src/main/java/study/redis/SpringRedisApplication.java
@@ -0,0 +1,13 @@
+package study.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringRedisApplication.class, args);
+  }
+
+}
---
+++ spring-redis/src/test/java/study/redis/SpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package study.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
