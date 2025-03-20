5dbe57c7aedc7eb9fa73dffc724d0c2d5e188d3d
이현수 <znight1020@naver.com>
2025-03-20 16:37:14 +0900

    init : spring transaction

---
+++ spring-transaction/src/test/java/study/transaction/SpringTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package study.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
