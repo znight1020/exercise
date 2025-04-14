09526677fd33d60fbda087f0ae19e014943dc659
이현수 <znight1020@naver.com>
2025-04-14 14:20:56 +0900

    [Typescript] 타입스크립트 리액트 사용

---
+++ typescript/section10_react/src/components/Editor.tsx
@@ -0,0 +1,23 @@
+import { useContext, useState } from "react";
+import { TodoDispatchContext, useTodoDispatch } from "../App";
+
+export default function Editor() {
+  const dispatch = useTodoDispatch();
+  const [text, setText] = useState("");
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const onClickButton = () => {
+    dispatch.onClickAdd(text);
+    setText("");
+  };
+
+  return (
+    <div>
+      <input value={text} onChange={onChangeInput} />
+      <button onClick={onClickButton}>추가</button>
+    </div>
+  );
+}
---
+++ typescript/section10_react/src/components/TodoItem.tsx
@@ -0,0 +1,19 @@
+import { useTodoDispatch } from "../App";
+import { Todo } from "../types";
+
+interface Props extends Todo {}
+
+export default function TodoItem(props: Props) {
+  const dispatch = useTodoDispatch();
+
+  const onClickButton = () => {
+    dispatch.onClickDelete(props.id);
+  };
+
+  return (
+    <div>
+      {props.id}번 : {props.content}
+      <button onClick={onClickButton}>삭제</button>
+    </div>
+  );
+}
